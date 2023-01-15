7d98912b517ddc335675c6d25ef8cc3fe95ffed9
deepak1051 <deepakpanwar1051@gmail.com>
2023-01-15 21:07:04 +0530

    initial commit

---
+++ light.js
@@ -0,0 +1,9 @@
+
+const button = document.querySelector('button')
+const img = document.querySelector('img')
+
+button.addEventListener('click', async () => {
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await res.json()
+  img.src = data.message
+})
